73b9b97624126fabec7569d98e337f5a0ecca4a0
Shawn Veader <shawn@veader.org>
2015-08-26 14:12:06 -0700

    Chapter 15: Challenge 1

--- app/src/main/java/com/sjm/cardiomems/android/crimintalintent/CrimeFragment.java
+++ app/src/main/java/com/sjm/cardiomems/android/crimintalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -166,11 +167,11 @@ public class CrimeFragment extends Fragment {
         mReportButton = (Button) v.findViewById(R.id.crime_report);
         mReportButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
-                i = Intent.createChooser(i, getString(R.string.send_report));
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                                    .setType("text/plain")
+                                    .setText(getCrimeReport())
+                                    .setSubject(getString(R.string.crime_report_subject))
+                                    .createChooserIntent();
                 startActivity(i);
             } });
 
